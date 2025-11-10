b9c559a559f3500c97173b12d3627ff9b4f9aed4
Celivra <celivra@Celivras-MacBook.local>
2025-11-10 19:28:30 +0800

    first commit

---
+++ src/main/java/com/celivra/transction/TransctionApplication.java
@@ -0,0 +1,13 @@
+package com.celivra.transction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransctionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/celivra/transction/TransctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.celivra.transction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
