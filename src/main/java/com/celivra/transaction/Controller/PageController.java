060e13c6a7dcfe53ed05b3c63273eac17dda32bb
Celivra <52xiaozhai@gmail.com>
2025-11-19 10:56:31 +0800

    update style 2

--- src/main/java/com/celivra/transaction/Controller/PageController.java
+++ src/main/java/com/celivra/transaction/Controller/PageController.java
@@ -1,8 +1,10 @@
 package com.celivra.transaction.Controller;
 
 import com.celivra.transaction.Pojo.Product;
+import com.celivra.transaction.Pojo.TransRecord;
 import com.celivra.transaction.Pojo.User;
 import com.celivra.transaction.Service.ProductService;
+import com.celivra.transaction.Service.TranRecordService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,13 +12,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class PageController {
 
     @Autowired
     ProductService productService;
+    @Autowired
+    TranRecordService tranRecordService;
 
     @GetMapping("/")
     public String IndexPage(Model model, HttpSession session) {
@@ -68,6 +76,36 @@ public class PageController {
         model.addAttribute("user", user);
         model.addAttribute("product", product);
         return "order";
+    }
+    @GetMapping("/TransRecord")
+    public String TransRecordPage(HttpSession session, Model model){
+        User user = (User) session.getAttribute("user");
+        List<TransRecord> buyList = tranRecordService.getTransRecordsByBuyer(user.getId());
+        List<TransRecord> sellList = tranRecordService.getTransRecordsBySeller(user.getId());
+
+        // 在TransRecordPage方法里
+        List<Map<String,Object>> buyRecords= new ArrayList<>();
+        List<Map<String,Object>> sellRecords= new ArrayList<>();
+
+        for(TransRecord tr : buyList){
+            Product product = productService.getProductById(tr.getProductId());
+            Map<String,Object> map = new HashMap<>();
+            map.put("record", tr);
+            map.put("productName", product.getName());
+            buyRecords.add(map);
+        }
+        for(TransRecord tr : sellList){
+            Product product = productService.getProductById(tr.getProductId());
+            Map<String,Object> map = new HashMap<>();
+            map.put("record", tr);
+            map.put("productName", product.getName());
+            sellRecords.add(map);
+        }
+
+        model.addAttribute("buy_records", buyRecords);
+        model.addAttribute("sell_records", sellRecords);
+        model.addAttribute("user", user);
+        return "t-record";
     }
 
 }
--- src/main/java/com/celivra/transaction/Controller/TransRecordController.java
+++ src/main/java/com/celivra/transaction/Controller/TransRecordController.java
@@ -1,60 +1,18 @@
 package com.celivra.transaction.Controller;
 
-import com.celivra.transaction.Pojo.Product;
 import com.celivra.transaction.Pojo.TransRecord;
-import com.celivra.transaction.Pojo.User;
-import com.celivra.transaction.Service.ProductService;
 import com.celivra.transaction.Service.TranRecordService;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 public class TransRecordController {
     @Autowired
     TranRecordService tranRecordService;
-    @Autowired
-    ProductService productService;
 
 
-    @GetMapping("/TransRecord")
-    public String TransRecordPage(HttpSession session, Model model){
-        User user = (User) session.getAttribute("user");
-        List<TransRecord> buyList = tranRecordService.getTransRecordsByBuyer(user.getId());
-        List<TransRecord> sellList = tranRecordService.getTransRecordsBySeller(user.getId());
-
-        // 在TransRecordPage方法里
-        List<Map<String,Object>> buyRecords= new ArrayList<>();
-        List<Map<String,Object>> sellRecords= new ArrayList<>();
-
-        for(TransRecord tr : buyList){
-            Product product = productService.getProductById(tr.getProductId());
-            Map<String,Object> map = new HashMap<>();
-            map.put("record", tr);
-            map.put("productName", product.getName());
-            buyRecords.add(map);
-        }
-        for(TransRecord tr : sellList){
-            Product product = productService.getProductById(tr.getProductId());
-            Map<String,Object> map = new HashMap<>();
-            map.put("record", tr);
-            map.put("productName", product.getName());
-            sellRecords.add(map);
-        }
-
-        model.addAttribute("buy_records", buyRecords);
-        model.addAttribute("sell_records", sellRecords);
-        model.addAttribute("user", user);
-        return "t-record";
-    }
     @PostMapping("/shipNow")
     @ResponseBody
     public ResponseEntity<String> shipNow(@RequestParam Integer recordId) {
